38138423c6df908e28f53a78da2f01b8c8622eed
saymaafroj <sza0063@auburn.edu>
2016-04-13 20:27:37 -0600

    Added files via upload

---
+++ REP2_FINAL.R
@@ -0,0 +1,23 @@
+library(qqman)
+GWAS <- read.csv("ladies.csv")
+
+#change factor to char.
+GWAS$CHR_ID <- as.character(GWAS$CHR_ID)
+
+#replace chrM with 17
+#GWAS[GWAS$CHR_ID=="chrM","CHR_ID"] <- "17"
+
+
+#char. to number
+GWAS$CHR_ID <- as.numeric(GWAS$CHR_ID)
+
+
+#do for plot group project allele frequencies
+manhattan(GWAS, chr="CHR_ID", bp="CHR_POS", p="REP2_FINAL", snp="SNP_ID_CURRENT", ylim=c(0, 1.2), logp=FALSE, ylab="change of allele frequency", genomewideline = FALSE, suggestiveline = FALSE, chrlabs=c(1:16, "chrM"), col =c(1:16, "red", "blues9"))
+
+
+
+png("rep2_manhattan.png", height=400, width=1000)
+manhattan(GWAS, chr="CHR_ID", bp="CHR_POS", p="REP2_FINAL", snp="SNP_ID_CURRENT", ylim=c(0, 1.2), logp=FALSE, ylab="change of allele frequency", genomewideline = FALSE, suggestiveline = FALSE, chrlabs=c(1:16, "chrM"), col =c(1:16, "red", "blues9"))
+abline(h=c(0.805), col="blue")
+dev.off()
